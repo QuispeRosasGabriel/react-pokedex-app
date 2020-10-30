bbf71677c5bc55e464570a9cf2a2b037ef69a630
Gabriel Quispe Rosas <gabriel.omar.quispe.rosas@everis.com>
2020-10-30 16:38:16 -0500

    limpiando consola

--- src/helpers/GetPokemon.js
+++ src/helpers/GetPokemon.js
@@ -4,7 +4,6 @@ const getPokemon = async (pokemon) => {
   const url = `https://pokeapi.co/api/v2/pokemon/${encodeURI(pokemon)}`;
   const resp = await axios.get(url);
   const { data } = await resp;
-  console.log("esta es data", data);
   const pokemonInfo = await data;
 
   return pokemonInfo;
